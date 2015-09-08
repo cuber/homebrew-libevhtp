153259def0319f14db9a7aaf906c022caf0658e6
cube <hourui.me@gmail.com>
2015-09-08 11:11:22 +0800

    upgrade to 1.2.10

--- libevhtp.rb
+++ libevhtp.rb
@@ -2,13 +2,14 @@ require 'formula'
  
 class Libevhtp < Formula
   homepage 'http://github.com/ellzey/libevhtp'
-  url 'https://github.com/ellzey/libevhtp/archive/1.2.9.tar.gz'
-  version '1.2.9'
-  sha1 'c4edfcfd1427db26e585ee7bc9bce7df4f10ec3d'
+  url 'https://github.com/ellzey/libevhtp/archive/1.2.10.tar.gz'
+  version '1.2.10'
+  sha1 'c481be3b26a4379e39d5952ff60a195f2c0e2176'
  
   head 'git://github.com/ellzey/libevhtp.git'
  
   depends_on 'cmake' => :build
+  depends_on 'openssl'
   depends_on 'libevent'
  
   def install
