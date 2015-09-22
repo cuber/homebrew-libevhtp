4c8a2657b09feedad6397a473a38adc30d4e6950
cube <hourui.me@gmail.com>
2015-09-23 05:06:17 +0800

    update patch

--- libevhtp-dev.rb
+++ libevhtp-dev.rb
@@ -20,6 +20,40 @@ class LibevhtpDev < Formula
 end
 
 __END__
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+index 93d3b44..d47ed46 100644
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -316,23 +316,23 @@ if(NOT INCLUDE_INSTALL_DIR)
+ endif()
+ 
+ install (TARGETS evhtp DESTINATION ${LIB_INSTALL_DIR})
+-install (FILES evhtp.h DESTINATION ${INCLUDE_INSTALL_DIR}/evhtp)
+-install (FILES htparse.h DESTINATION ${INCLUDE_INSTALL_DIR}/evhtp)
+-install (FILES ${CMAKE_CURRENT_BINARY_DIR}/evhtp-config.h DESTINATION ${INCLUDE_INSTALL_DIR}/evhtp)
++install (FILES evhtp.h DESTINATION ${INCLUDE_INSTALL_DIR})
++install (FILES htparse.h DESTINATION ${INCLUDE_INSTALL_DIR})
++install (FILES ${CMAKE_CURRENT_BINARY_DIR}/evhtp-config.h DESTINATION ${INCLUDE_INSTALL_DIR})
+ 
+ if (NOT EVHTP_DISABLE_EVTHR)
+-	install (FILES evthr.h DESTINATION ${INCLUDE_INSTALL_DIR}/evhtp)
++	install (FILES evthr.h DESTINATION ${INCLUDE_INSTALL_DIR})
+ endif()
+ 
+ if (NOT EVHTP_DISABLE_REGEX)
+ 		if (NOT HAS_SYS_ONIG)
+-				install (FILES oniguruma/onigposix.h DESTINATION ${INCLUDE_INSTALL_DIR}/evhtp)
++				install (FILES oniguruma/onigposix.h DESTINATION ${INCLUDE_INSTALL_DIR})
+ 		endif()
+ endif()
+ 
+ if (NOT HAVE_SYS_QUEUE)
+ 		install(FILES ${CMAKE_CURRENT_BINARY_DIR}/compat/sys/queue.h
+-				DESTINATION ${INCLUDE_INSTALL_DIR}/evhtp/sys)
++				DESTINATION ${INCLUDE_INSTALL_DIR}/sys)
+ endif()
+ 
+ IF (WIN32)
 diff --git a/evhtp.c b/evhtp.c
 index 6ff6b90..4fde2fe 100644
 --- a/evhtp.c
